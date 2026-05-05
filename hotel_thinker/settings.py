4101fb6ca4d0b730942db27a244344c120bfeb11
atwanzireT <tatwanzire@gmail.com>
2026-05-06 00:47:14 +0300

    worked on allowed origin

--- hotel_thinker/settings.py
+++ hotel_thinker/settings.py
@@ -169,7 +169,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'https://hotelthinker.site', 
+    'https://www.hotelthinker.site'
 ]
+
 
 CORS_ALLOW_HEADERS = [
     "accept",
